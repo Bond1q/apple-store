e81c80520dfd2ab74b166c44b3b885ea91aad6e1
Bondi <kirkizon5@gmail.com>
2022-01-10 14:52:58 +0200

    public url

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store';
 ReactDOM.render(
 	<React.StrictMode>
 
-		<HashRouter>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<Provider store={store}>
 				<App />
 			</Provider>
