0629a194bce1bc576104b8c463e1a94bfd2f5751
Bondi <kirkizon5@gmail.com>
2022-01-10 15:36:03 +0200

    2

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = '/db.json'
+const url = 'https://bond1q.github.io/apple-store/db.json'
 
 export const getItemsFromDB = async () => {
 	const items = await axios.get(url)
