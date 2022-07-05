6b89baf5f10fde5fc236fbfb893d9f4995b2aaf7
Bondi <kirkizon5@gmail.com>
2022-07-05 17:08:34 +0300

    image loader

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import "../styles/productItem.scss"
 import classNames from 'classnames'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css';
+// import 'react-lazy-load-image-component/src/effects/black-and-white.css';
+// import 'react-lazy-load-image-component/src/effects/opacity.css';
+
 const ProductItem = ({ name, color, price, img, size, isVisible }) => {
 
 	function numberWithSpaces(x) {
@@ -27,7 +32,12 @@ const ProductItem = ({ name, color, price, img, size, isVisible }) => {
 			to={url}>
 			<div className="productColumn">
 				<div className="productPhoto">
-					<img src={img} alt="" />
+					{/* <img src={img} alt="" /> */}
+					<LazyLoadImage
+						src={img}
+						width={233}
+						effect="blur"
+					/>
 				</div>
 				<div className="productName">
 					{name}<br /> {size} ({color})
